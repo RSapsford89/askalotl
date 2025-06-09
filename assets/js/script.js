15fd712551f497b6a5cbb3eb93df85f10c8d6018
RSapsford89 <treborwar@live.co.uk>
2025-06-09 21:34:46 +0100

    start JS animalSelection(), created CSS classes for hiding content

--- assets/js/script.js
+++ assets/js/script.js
@@ -121,4 +121,12 @@ function updateImage() {
     makeImageFollow();
 }
 
+function animalSelection(){
+    const image = document.getElementById("imgSelect");
+    const paragraphs = document.getElementsByClassName("introText");
+
+    image.addEventListener('click',()=>{
+
+    });
+}
 
